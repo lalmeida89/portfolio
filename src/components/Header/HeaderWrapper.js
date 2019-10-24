7908d85cee30550ac3c8dba4a94fc06a93f8cbf1
Luiz <lalmeida89@gmail.com>
2019-10-23 21:14:16 -0400

    new color scheme

--- src/components/Header/HeaderWrapper.js
+++ src/components/Header/HeaderWrapper.js
@@ -4,8 +4,8 @@ export const HeaderWrapper = props => {
   return (
     <div id='header'>
       <div id='header-description'>
-        <h1> Hi, I'm Luiz </h1>
-        <h2> A React developer from Brooklyn, New York.
+        <h1 id='intro-header'> Hello </h1>
+        <h2> I'm Luiz, a React developer from Brooklyn, New York.
          I create dynamic websites that help businesses reach
          more customers and look better online. </h2>
       </div>
